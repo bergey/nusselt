14e47bee4900534c69db9b4e13a7cc9492d1c717
Daniel Bergey <bergey@alum.mit.edu>
2011-11-09 23:26:56 -0500

    onclick behavior

--- line_dots.js
+++ line_dots.js
@@ -2,7 +2,7 @@ data = [10, 20, 15, 3, 7, 19, 12, 2, 8]
 var w = 400,
     h = 200,
 margin = 10
-y = d3.scale.linear().domain([0,25]).range([0, h]),
+y = d3.scale.linear().domain([0,25]).range([h, 0])
 x = d3.scale.linear().domain([0,10]).range([margin, w-margin])
 
 // create svg and transform group for line graph
@@ -20,19 +20,22 @@ var line = d3.svg.line()
 
 g.append("svg:path")
     .attr("d", line(data))
-    .attr("style", "stroke: blueviolet");
 
 var node = g.selectAll("path.dot")
         .data(data)
-        .enter().append("svg:path")
-        .attr("class", "dot")
+	.enter().append("svg:circle")
+	.attr("cx", function(d, i) { return x(i) })
+	.attr("cy", function(d) { return y(d) })
+	.attr("r", 5)
         .style("fill", "white")
         .style("stroke-width", "1.5px")
         .attr("stroke", "#9acd32")
-        .attr("transform", function(d,i) { return "translate(" + x(i) + "," + y(d) + ")"; })
         .attr("d", d3.svg.symbol())
         .on("mouseover", function(d,i) {
-            d3.select(this).transition().duration(300).style("fill","#00ffff"); })
+            d3.select(this).transition().duration(300).style("fill","lightgreen"); })
         .on("mouseout", function(d,i) {
-            d3.select(this).transition().duration(300).style("fill","white"); });
+            d3.select(this).transition().duration(300).style("fill","white").attr("r", 5); })
+	.on("click", function(d) {
+		d3.select(this).transition().duration(300).attr("r", 20)
+	    d3.select("span.graphval").text(d)})
 
