7980ddd745a4e0733971d0992004e3627dc2f716
Christalee Bieber <cbieber@alum.mit.edu>
2011-12-21 20:17:23 -0500

    implemented double-click for zoom

--- line_svg.js
+++ line_svg.js
@@ -4,27 +4,32 @@ var w = 800,
 h = 500,
 margin = 25,
 y = d3.scale.linear().domain([100, 40]).range([0 + margin, h - margin]),
-x = d3.scale.linear().domain([0, 8760]).range([0 + margin, w - margin])
+x = d3.scale.linear().domain([0, 8760]).range([0 + margin, w - margin]), 
+tx = function(d) { return "translate(" + x(d) + ",0)"; },
+ty = function(d) { return "translate(0," + y(d) + ")"; }
 
 // create svg and transform group for line graph
 var vis = d3.select("body")
     .append("svg:svg")
     .attr("width", w)
     .attr("height", h)
+    .attr("pointer-events", "all")
 
 var defs = vis.append("svg:defs");
 
+//add a clipping path to prevent data from graphing outside the axes
 defs.append("svg:clipPath")
-	.attr("id", "databox")
-	.append("svg:rect")
-	.attr("x", margin)
-	.attr("y", margin)
-	.attr("width", w - 2*margin)
-	.attr("height", h - 2*margin);
+    .attr("id", "databox")
+    .append("svg:rect")
+    .attr("x", margin)
+    .attr("y", margin)
+    .attr("width", w - 2*margin)
+    .attr("height", h - 2*margin);
 
 var g = vis.append("svg:g")
-// skipping this method of moving the origin
-// .attr("transform", "translate(0, 200)");
+    .call(d3.behavior.zoom()
+        .extent([[0, w], [0, h], [0, 10]])
+        .on("zoom", redraw));
 
 function createLine(dataname) { return function (json) {
 // create and append the line itself
@@ -33,14 +38,15 @@ var line = d3.svg.line()
     .y(function(d) { return y(d); });
 
 g.append("svg:path")
-    .attr("d", line(json))
+    .data([json])
+    .attr("d", line)
 	.attr("id", dataname)
 	.attr("style", "stroke: red")
 	.attr("clip-path", "url(#databox)"); }}
 
 // data is Miami TMY3 outdoor dry bulb
 var to = d3.json("json/z1h100s1rh50v1-To.json", createLine("Miami"))
-//and another random dataset
+//and another, from Indianapolis
 var t2 = d3.json("json/z5h100s1rh50v1-To.json", createLine("Indianapolis"))
 
 // x axis line
@@ -57,25 +63,16 @@ g.append("svg:line")
     .attr("x2", margin)
     .attr("y2", h-margin)
 
+redraw();
+
 // axis labels, scaled to the months of the year
-g.selectAll(".xLabel")
-    .data([24*day for each (day in [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365])])
-    .enter().append("svg:text")
-    .attr("class", "xLabel")
-    .text(function(d, i) {return ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan"][i]; })
-    .attr("x", x)
-    .attr("y", h-5)
-    .attr("text-anchor", "middle")
+//g.selectAll(".xLabel")
+//    .data([24*day for each (day in [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365])])
+//    .enter().append("svg:text")
+//    .attr("class", "xLabel")
+//    .text(function(d, i) {return ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan"][i]; })
 
-g.selectAll(".yLabel")
-    .data(y.ticks(4))
-    .enter().append("svg:text")
-    .attr("class", "yLabel")
-    .text(String)
-    .attr("x", 0)
-    .attr("y", y)
-    .attr("dy", 4)
-
+//changes the color of each datapath based on its dropdown selector
 function colorChange(selectObj, dataname) {
 	var idx = selectObj.selectedIndex;
 	var color = selectObj.options[idx].value;
@@ -83,6 +80,7 @@ function colorChange(selectObj, dataname) {
 	d3.select("#" + dataname)
 		.attr("style", "stroke: " + color)}
 
+//switches each datapath on or off based on its tickybox
 function showHide(tickyBox, dataname) {
 	var idx = tickyBox.checked
 	if (idx){
@@ -91,3 +89,61 @@ function showHide(tickyBox, dataname) {
 	else{
 		d3.select("#" + dataname)
 			.attr("display", "none");}}
+
+// Taken from mbostock's zoom-pan.html example
+function redraw() {
+  if (d3.event) d3.event.transform(x, y);
+
+  var fx = x.tickFormat(12),
+      fy = y.tickFormat(4);
+
+  // Regenerate x-ticks…
+  var gx = vis.selectAll("g.xLabel")
+      .data(x.ticks(12), String)
+      .attr("transform", tx);
+
+  gx.select("text")
+      .text(fx);
+
+  var gxe = gx.enter().insert("g")
+      .attr("class", "xLabel")
+      .attr("transform", tx);
+
+  gxe.append("text")
+      .attr("y", h - 5)
+//      .attr("dy", "1em")
+      .attr("text-anchor", "middle")
+      .text(fx);
+
+  gx.exit().remove();
+
+  // Regenerate y-ticks…
+  var gy = vis.selectAll("g.yLabel")
+      .data(y.ticks(4), String)
+      .attr("transform", ty);
+
+  gy.select("text")
+      .text(fy);
+
+  var gye = gy.enter().insert("g")
+      .attr("class", "yLabel")
+      .attr("transform", ty);
+
+  gye.append("text")
+      .attr("x", 0)
+      .attr("dy", 4)
+//      .attr("text-anchor", "end")
+      .text(fy);
+
+  gy.exit().remove();
+
+vis.selectAll("path")
+    .attr("d", d3.svg.line()
+        .x(function(d,i) { return x(i); })
+        .y(function(d) { return y(d); }));
+}
+
+        
+//function rescale() {
+//	x = x.domain([d3.select("#x-bottom").value, d3.select("#x-top").value]);
+//	y = y.domain([d3.select("#y-top").value, d3.select("#y-bottom").value]);}
